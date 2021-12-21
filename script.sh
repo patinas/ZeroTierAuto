f72b53116e65e96ac8ec5a909adf78b9aa4f0e20
patinas <andreas.patinas@gmail.com>
2021-12-21 08:26:18 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+while true; do ping -c1 www.google.com > /dev/null && break; done
+
+sudo curl -s https://install.zerotier.com | sudo bash
+sudo zerotier-cli join 9f77fc393e977c15
+exit
