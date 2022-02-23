fab5f7e44285d6a9eede7d69dd4868db13e901fa
patinas <andreas.patinas@gmail.com>
2022-02-23 08:57:09 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,10 +2,11 @@
 
 while true; do ping -c1 www.google.com > /dev/null && break; done
 
+sudo chmod +x *
+
 sudo apt update && sudo apt upgrade -y
 
 sudo curl -s https://install.zerotier.com | sudo bash
 sudo zerotier-cli join 9f77fc393e977c16
 
-sudo chmod +x ~/ZeroTierAuto/script.sh
 exit
